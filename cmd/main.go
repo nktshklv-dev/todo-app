a4508d7c82b1c1041d7f44becfa1a8895be694c6
nktshklv-dev <nikitashakalov@gmail.com>
2025-09-09 21:08:56 +0300

    architecture setup

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,16 @@ import (
 	"log"
 	todo "todo-app"
 	"todo-app/pkg/handler"
+	"todo-app/pkg/repository"
+	"todo-app/pkg/service"
 )
 
 func main() {
+	repo := repository.NewRepository()
+	services := service.NewService(repo)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	handlers := new(handler.Handler)
 
 	if err := srv.Run(("8080"), handlers.InitRoutes()); err != nil {
 		log.Fatal(err)
